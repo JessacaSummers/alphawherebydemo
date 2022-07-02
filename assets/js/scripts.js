88ee9442611a86c695c8a30ce12d41eb86d6b809
Jess <jessacasummers@gmail.com>
2022-07-02 09:51:22 -0500

    Avoided CORS issue entirely, testing audience

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(function(){
-      const API_KEY = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmFwcGVhci5pbiIsImF1ZCI6Imh0dHBzOi8vYXBpLmFwcGVhci5pbi92MSIsImV4cCI6OTAwNzE5OTI1NDc0MDk5MSwiaWF0IjoxNjU2NDE4MTk1LCJvcmdhbml6YXRpb25JZCI6MTYzMzM0LCJqdGkiOiJmYzA3Nzg3My1mNDZkLTRiNDEtOWMyZS02MWFhNjFlNmFlMjcifQ.qeXf5ZwgcZ7x9LlBx8tOrvcyHng3bUv1raAkptSbons";
+      const API_KEY = '[Removed]'
       //const fetch = require('cross-fetch');
 
      // console.log("API_KEY:", API_KEY);
@@ -8,9 +8,8 @@ $(function(){
             method: "POST",
             mode: "no-cors",
             headers: {
-                  "Authorization": API_KEY,
+                  "Authorization": `Bearer` + API_KEY,
                   "Content-Type": "application/json",
-                  //"Access-Control-Allow-Origin": *
             },
             body: JSON.stringify({
                   endDate: "2022-07-04 00:00:00",
